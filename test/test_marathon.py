18fce191777ab3d264990495a3551ec2a46dace8
SunPowered <tim@vanboxtel.ca>
2013-03-26 15:32:17 -0400

    Triangular rotations in the test module

--- test/test_marathon.py
+++ test/test_marathon.py
@@ -8,8 +8,9 @@ base_dir = os.path.dirname(__file__)
 marathon_script = os.path.abspath(os.path.join(base_dir, os.pardir, "src", "marathon.py"))
 
 test_data_directory = os.path.join(base_dir, 'initialGraphs')
-single_data_file = os.path.join(test_data_directory, "1B36_A_Graph.pdb")
-
+filename = "1B36_A_Graph.pdb"
+#filename = "1GID_A_Graph.pdb"
+single_data_file = os.path.join(test_data_directory, filename)
 output_directory = os.path.join(base_dir, "output")
 
 if os.path.isdir(output_directory):
